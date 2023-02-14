ccbd91688c4f5582b2afeeb73a1ecc0d210dc112
Gabriel Setticase <gabrielsetticase@gmail.com>
2023-02-14 12:15:34 -0300

    ejercicios 1/2/3

--- ejercicio2.js
+++ ejercicio2.js
@@ -1,1 +1,41 @@
-fewfewfewfwe
+// EJERCICIO 1
+const numeroRandom = Math.floor((Math.random() * (100 - 1 + 1)) + 1);
+let resto = numeroRandom % 2
+
+if (resto ==0) {
+    console.log("El numero " + numeroRandom + " es par")
+    } else { console.log("el numero " + numeroRandom + " es impar")}
+
+// EJERCICIO 2
+
+let variable1 = "tren"
+let variable2 = "edificio"
+
+if (variable1.length > variable2.length) {
+    console.log ("la variable 1 tiene mas palabras que la variable 2")
+} else if (variable1.length == variable2.length) {
+      console.log ("ambas variables tienen la misma cantidad de palabras")  
+} else {
+    console.log ("la variable 2 tiene mas palabras que la variable 1")
+}
+
+// EJERCICIO 3
+
+for (let i = 1; i < 11; i++) {
+    let resultado = i * 9
+    console.log ("9 x " + i + "= " + resultado)
+}
+
+// EJERCICIO 4
+
+const multiplicar = (num1) => num1 * 23
+let resultado = multiplicar (10)
+console.log ("el resultado es " + resultado)
+
+// EJERCICIO 5
+
+lista = []
+while (lista.length < 10) {
+    lista.push (Math.floor((Math.random() * (100 - 1 + 1)) + 1))
+}
+console.log (lista)
---
+++ ejercicio3.js
@@ -0,0 +1,47 @@
+const readLine = require ("readline-sync")
+
+// ejercicio 1
+// const userName = readLine.question("Ingrese su nombre: ")
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(userName)    
+// }
+
+// ejercicio 2
+
+// const edad = readLine.question("¿Que edad tenes? ")
+// for (let i = 0; i < edad;) {
+//     console.log(++i)    
+// }
+
+// ejercicio 3
+
+// const numeroPositivo = readLine.question ("Ingrese un numero positivo")
+// miLista = []
+// if (numeroPositivo<=0) {
+//     console.log ("numero ingresado incorrecto")
+// } else {
+//   for (let i = 0; i < numeroPositivo; i++) {
+//     if (i % 2 !==0) {
+//         miLista.push (i)
+//     }   
+//     }
+// }
+// console.log(miLista.join(" , "))
+
+// ejercicio 4
+
+// const numeroEntero = readLine.question("Ingrese un numero ")
+// let llave = "*"
+// for (let i = 0; i < numeroEntero; i++) {
+//     console.log (llave) 
+//     llave = llave + "*"
+// }
+
+// ejercicio 5 
+
+// let palabra = readLine.question("Escriba una palabra ")
+
+// for (let i = palabra.length; i >0; i--) {
+//     console.log (palabra[i-1])    
+// }
