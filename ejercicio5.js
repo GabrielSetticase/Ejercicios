107c94f447f154c94e5bb5d0901458b053f67bcd
Gabriel Setticase <gabrielsetticase@gmail.com>
2023-02-24 13:17:13 -0300

    entrega de ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,40 @@
+// function saludo(nombre) {
+//       console.log("Hola ", nombre)
+// }
+// if (process.argv[2] === "saludar") {
+//     (saludo(process.argv[3]))
+// }
+
+// ejercicio 2
+
+// const readline = require("readline-sync")
+
+// datosPersona = {
+//     nombre: readline.question("Ingrese su nombre "),
+//     apellido: readline.question("Ingrese su apellido "),
+//     edad: readline.question("Ingrese su edad ")
+// }
+// console.log(datosPersona);
+
+//ejercicio 3
+
+// miLista = ["perro", "gato", "elefante", "jirafa"]
+// miLista2= ["agua", "aceite", "harina"]
+
+// function buscado(lista, elementoBuscado) {
+//     if (lista.includes(elementoBuscado)) {
+//         console.log("elemento encontrado en la posición ", lista.indexOf(elementoBuscado));
+//     }    
+// }
+// buscado(miLista, "gato")
+// buscado(miLista2, "harina")
+
+// ejercicio 4
+
+// let lista = [1,2,3,4,5]
+
+// function revertir(lista) {
+//     let arrayInvertido = lista.reverse()
+//     console.log(arrayInvertido);
+// }
+// revertir(lista)
